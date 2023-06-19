1ab66855a3d455ee744d150cb9e26d0d091b2a24
Rafael <rafa.8404@hotmail.com>
2023-06-19 15:31:32 -0300

    implementation Float Action Button on Main Activity

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.orgs.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -7,13 +8,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
 import com.example.orgs.ui.adapter.ListaCarrosAdapter
 import com.example.orgs.model.Cars
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recycleView = findViewById<RecyclerView>(R.id.recycleView)
+        initView()
+        navigateFormularioCarros()
+    }
+
+    private fun initView() {
+        val recycleView: RecyclerView = findViewById(R.id.recycleView)
         recycleView.adapter = ListaCarrosAdapter(context = this, cars = listOf(
             Cars(
                 "Jetta",
@@ -27,6 +34,13 @@ class MainActivity : AppCompatActivity() {
             ),
         ))
         recycleView.layoutManager = LinearLayoutManager(this)
+    }
 
+    private fun navigateFormularioCarros() {
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener{
+            val intent = Intent(this, FormularioCarrosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
