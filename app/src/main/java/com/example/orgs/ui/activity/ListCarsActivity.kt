28f94dcd1542d0f11543e4bfaf4d40934ab9ae46
Rafael <rafa.8404@hotmail.com>
2023-06-28 17:26:42 -0300

    adding room database (testing on main thread)

--- app/src/main/java/com/example/orgs/ui/activity/ListCarsActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ListCarsActivity.kt
@@ -1,13 +1,20 @@
 package com.example.orgs.ui.activity
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
 import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.example.orgs.dao.ProductsDao
+import com.example.orgs.database.AppDataBase
 import com.example.orgs.databinding.ActivityListCarsBinding
+import com.example.orgs.model.Car
 import com.example.orgs.ui.adapter.ListaCarrosAdapter
+import java.math.BigDecimal
 
 class ListCarsActivity : AppCompatActivity() {
 
@@ -22,11 +29,29 @@ class ListCarsActivity : AppCompatActivity() {
         setContentView(binding.root)
         initRecycleView()
         navigateFormsCars()
+
+
+        val db = Room.databaseBuilder(
+            this,
+            AppDataBase::class.java,
+            "cars.db"
+        ).allowMainThreadQueries()
+            .build()
+        val carsDao = db.carDao()
+        carsDao.insertAll(
+            Car(
+                name = "teste 1",
+                modelCar = "teste 1",
+                price = BigDecimal("10")
+            )
+        )
+        adapter.notifyChanged(carsDao.getAll())
     }
+
 
     override fun onResume() {
         super.onResume()
-        adapter.notifyChanged(dao.showCars())
+       // adapter.notifyChanged(dao.showCars())
     }
 
     private fun initRecycleView() {
