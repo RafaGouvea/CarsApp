6723e01e431202223386f1ed3a9022e6ba6b6f60
Rafael <rafa.8404@hotmail.com>
2023-07-01 01:14:39 -0300

    Starting to add menu itens

--- app/src/main/java/com/example/orgs/ui/activity/DetailCarActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/DetailCarActivity.kt
@@ -2,6 +2,10 @@ package com.example.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.example.orgs.R
 import com.example.orgs.databinding.ActivityDetailCarBinding
 import com.example.orgs.extensions.formatToBrazilianReal
 import com.example.orgs.extensions.loadImgView
@@ -16,6 +20,24 @@ class DetailCarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         loadProduct()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_details_car, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_detail_car_edit -> {
+                Log.i("Teste", "Edit")
+            }
+            R.id.menu_detail_car_delete -> {
+                Log.i("Teste", "Delete")
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun loadProduct() {
--- app/src/main/java/com/example/orgs/ui/activity/FormCarsActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormCarsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.orgs.ui.activity
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import com.example.orgs.R
 import com.example.orgs.database.AppDataBase
 import com.example.orgs.databinding.ActivityFormCarsBinding
 import com.example.orgs.extensions.loadImgView
